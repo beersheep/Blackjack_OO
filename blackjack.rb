fcbc8a213d0b1764344980561e449f068c3e65ea
beersheep <beersheep0109@gmail.com>
2015-11-06 16:31:43 +0800

    manage to set up a game

--- blackjack.rb
+++ blackjack.rb
@@ -1,45 +1,55 @@
 require "pry"
 
 class Deck
+  attr_accessor :deck
   def initialize
     value = %w(2 3 4 5 6 7 8 9 10 J Q K A)
     suit = ["Spare", "Diamond", "Heart", "Club"]
     @deck = (value.product(suit))
   end
+
+  # def shuffle!
+  #   @deck.shuffle!
+  # end
+  def deal
+    @deck.pop
+  end
 end
 
 class Player
+  attr_accessor :hand
   def initialize(name)
     @name = name
     @hand = []
   end
 end
 
-class Dealer
-  def initialize
-    @name = "dealer"
-    @hand = []
-  end
+class Human < Player
+end 
+
+class Dealer < Player
+end
 
 class Game
   def initialize
     @deck = Deck.new
     @player = Player.new("Roy")
-    @dealer = Dealer.new 
+    @dealer = Dealer.new("Dealer")
   end
 
   def deal_a_card
-    4.times do 
-      @deck.shuffle!
-    end
-
-  end
+    @player.hand << @deck.deal
   end
 
   def start_game
     4.times do 
       @deck.shuffle!
     end
-    
+    deal_a_card
+    @dealer.hand << @deck.deal
+    deal_a_card
+    @dealer.hand << @deck.deal 
   end
 end
+
+binding.pry
