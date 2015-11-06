36da869208f22a6cf299d02b6ace067a5803908e
beersheep <beersheep0109@gmail.com>
2015-11-06 17:09:37 +0800

    refactor setup game

--- blackjack.rb
+++ blackjack.rb
@@ -1,20 +1,21 @@
 require "pry"
 
 class Deck
-  attr_accessor :deck
   def initialize
     value = %w(2 3 4 5 6 7 8 9 10 J Q K A)
     suit = ["Spare", "Diamond", "Heart", "Club"]
-    @deck = (value.product(suit))
+    @deck = value.product(suit)
   end
 
-  # def shuffle!
-  #   @deck.shuffle!
-  # end
+  def shuffle!
+    @deck.shuffle!
+  end
+
   def deal
     @deck.pop
   end
 end
+
 
 class Player
   attr_accessor :hand
@@ -22,6 +23,7 @@ class Player
     @name = name
     @hand = []
   end
+  
 end
 
 class Human < Player
@@ -31,25 +33,42 @@ class Dealer < Player
 end
 
 class Game
+
   def initialize
     @deck = Deck.new
     @player = Player.new("Roy")
     @dealer = Dealer.new("Dealer")
+    @current_caller = @player
   end
 
   def deal_a_card
-    @player.hand << @deck.deal
+    if @current_caller == @player
+      @player.hand << @deck.deal
+    else 
+      @dealer.hand << @deck.deal
+    end
   end
 
-  def start_game
+  def change_caller
+    if @current_caller == @player
+      @current_caller = @dealer
+    else 
+      @current_caller = @player
+    end
+  end
+
+  def setup_game
     4.times do 
       @deck.shuffle!
     end
-    deal_a_card
-    @dealer.hand << @deck.deal
-    deal_a_card
-    @dealer.hand << @deck.deal 
+    loop do 
+      deal_a_card
+      change_caller
+      break if @dealer.hand.size == 2
+    end
+
   end
+
 end
 
 binding.pry
