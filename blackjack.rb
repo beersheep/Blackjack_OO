4a3b4ee35dd8a12eda470ca3b652a1bdd039587e
beersheep <beersheep0109@gmail.com>
2015-11-06 22:51:37 +0800

    add count total function

--- blackjack.rb
+++ blackjack.rb
@@ -4,7 +4,7 @@ class Deck
   def initialize
     value = %w(2 3 4 5 6 7 8 9 10 J Q K A)
     suit = ["Spare", "Diamond", "Heart", "Club"]
-    @deck = value.product(suit)
+    @deck = suit.product(value)
   end
 
   def shuffle!
@@ -22,8 +22,27 @@ class Player
   def initialize(name)
     @name = name
     @hand = []
+    @total = 0 
   end
-  
+
+  def calculate_total
+    @hand.each do |card|
+      if card[1] == "A"
+        @total += 11
+      elsif card[1].to_i == 0
+        @total += 10
+      else
+        @total += card[1].to_i
+      end  
+    end
+    if @total > 21 
+      @hand.select {|card| card[1] == "A"}.size.times do 
+        @total -= 10
+      end
+    end
+    @total
+  end
+
 end
 
 class Human < Player
@@ -41,22 +60,6 @@ class Game
     @current_caller = @player
   end
 
-  def deal_a_card
-    if @current_caller == @player
-      @player.hand << @deck.deal
-    else 
-      @dealer.hand << @deck.deal
-    end
-  end
-
-  def change_caller
-    if @current_caller == @player
-      @current_caller = @dealer
-    else 
-      @current_caller = @player
-    end
-  end
-
   def setup_game
     4.times do 
       @deck.shuffle!
@@ -66,9 +69,40 @@ class Game
       change_caller
       break if @dealer.hand.size == 2
     end
+  end
 
+  def display_table_info
+    puts "Dealer has a #{@dealer.hand[1]}"
+    puts "================================"
+    puts "You have #{@player.hand[0]} and #{@player.hand[1]}, total of #{@player.calculate_total}"
+  end
+
+  def play
+    setup_game
+    display_table_info
+  end
+
+
+  private
+
+  def change_caller
+    if @current_caller == @player
+      @current_caller = @dealer
+    else 
+      @current_caller = @player
+    end
+  end
+
+  def deal_a_card
+    if @current_caller == @player
+      @player.hand << @deck.deal
+    else 
+      @dealer.hand << @deck.deal
+    end
   end
 
 end
 
-binding.pry
+Game.new.play
+
+# binding.pry
