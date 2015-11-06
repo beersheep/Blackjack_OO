f9d0afc01a0c8578246c5aab6f930ea3ecfc2a44
beersheep <beersheep0109@gmail.com>
2015-11-06 16:14:59 +0800

    added player, dealer and game method

--- blackjack.rb
+++ blackjack.rb
@@ -8,5 +8,38 @@ class Deck
   end
 end
 
-binding.pry
+class Player
+  def initialize(name)
+    @name = name
+    @hand = []
+  end
+end
 
+class Dealer
+  def initialize
+    @name = "dealer"
+    @hand = []
+  end
+
+class Game
+  def initialize
+    @deck = Deck.new
+    @player = Player.new("Roy")
+    @dealer = Dealer.new 
+  end
+
+  def deal_a_card
+    4.times do 
+      @deck.shuffle!
+    end
+
+  end
+  end
+
+  def start_game
+    4.times do 
+      @deck.shuffle!
+    end
+    
+  end
+end
