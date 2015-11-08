733559f879979bb559649bbf887f08b260fa7113
beersheep <beersheep0109@gmail.com>
2015-11-08 20:30:02 +0800

    added attr_reader and delete @ symbol

--- blackjack.rb
+++ blackjack.rb
@@ -53,6 +53,7 @@ end
 
 class Game
   BLACKJACK = 21
+  attr_accessor :player, :dealer, :deck
 
   def initialize
     @deck = Deck.new
@@ -73,33 +74,31 @@ class Game
   private
 
   def setup_game
-    4.times do 
-      @deck.shuffle!
-    end
+    4.times { @deck.shuffle! }
     loop do 
-      deal_a_card(@player)
-      deal_a_card(@dealer)
-      break if @dealer.hand.size == 2
+      deal_a_card(player)
+      deal_a_card(dealer)
+      break if dealer.hand.size == 2
     end
   end
 
   def display_table_info
-    puts "Dealer has a #{@dealer.hand[1]}"
+    puts "Dealer has a #{dealer.hand[1]}"
     puts "================================"
-    puts "You have #{@player.hand[0]} and #{@player.hand[1]}, total of #{@player.total}"
+    puts "You have #{player.hand[0]} and #{player.hand[1]}, total of #{@player.total}"
   end
 
   def player_turn
     loop do 
-      if @player.blackjack?
+      if player.blackjack?
         puts "Blackjack! You win!"
         play_again?
       end
       puts "Would you like to hit?(Y/N)"
       if gets.chomp.downcase == "y"
-        deal_a_card(@player)
-        puts "You get a #{@player.hand.last}, total of #{@player.total}"
-        if @player.busted?
+        deal_a_card(player)
+        puts "You get a #{player.hand.last}, total of #{player.total}"
+        if player.busted?
           puts "You are busted!"
           play_again?
         end
@@ -111,7 +110,7 @@ class Game
 
   def dealer_turn
     system "clear"
-    puts "Dealer have #{@dealer.hand[0]} and #{@dealer.hand[1]}, total of #{@dealer.total}"
+    puts "Dealer have #{dealer.hand[0]} and #{dealer.hand[1]}, total of #{dealer.total}"
 
     loop do 
       if @dealer.blackjack?
@@ -122,7 +121,7 @@ class Game
         sleep 0.5
         puts "=> Dealer chooses to hit."
         deal_a_card(@dealer)
-        puts "Dealer gets a #{@dealer.hand.last}, total of #{@dealer.total}"
+        puts "Dealer gets a #{dealer.hand.last}, total of #{dealer.total}"
         if @dealer.busted?
           puts "Dealer busted! You win!"
           play_again?
@@ -138,17 +137,17 @@ class Game
     system "clear"
     puts "Player's hand are"
     @player.hand.each {|card| puts "#{card}"}
-    puts "the total of #{@player.total}"
+    puts "the total of #{player.total}"
     puts "=================================="
     puts "Dealer's hand are"
     @dealer.hand.each {|card| puts "#{card}"}
-    puts "the total of #{@dealer.total}"
+    puts "the total of #{dealer.total}"
   end
 
   def check_winner
-    if @player.total > @dealer.total
+    if player.total > dealer.total
       puts "You win!"
-    elsif @dealer.total > @player.total
+    elsif dealer.total > player.total
       puts "Dealer wins!"
     else 
       puts "It's a push!"
@@ -157,7 +156,7 @@ class Game
   end
 
   def deal_a_card(player)
-    player.hand << @deck.deal
+    player.hand << deck.deal
   end
 
   def play_again?
@@ -174,9 +173,9 @@ class Game
 
   def replay 
     system "clear"
-    @deck = Deck.new
-    @player.hand.clear
-    @dealer.hand.clear
+    self.deck = Deck.new
+    player.hand.clear
+    dealer.hand.clear
     play
   end
   
